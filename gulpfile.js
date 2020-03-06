18c7182dc16ac2d9dbae33a7526570a7f5d1e35c
MatheusH <matheushds.br@gmail.com>
2020-03-06 11:50:56 -0300

    images added

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,16 @@
-'use strict';
-
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 sass.compiler = require('node-sass');
 
-gulp.task('sass', function () {
+gulp.task('sass', gulp.series(function () {
   return gulp.src('src/sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('src/css'));
-});
+}));
 
-gulp.task('sass:watch', function () {
-  gulp.watch('src/sass/**/*.scss', ['sass']);
-});
+gulp.task('watch', gulp.series(function () {
+  gulp.watch('src/sass/**/*.scss', gulp.parallel(['sass']));
+}));
+
+gulp.task('default', gulp.series(['sass', 'watch']))
